45323413f8fa58bab12bfa3273daf3f87f426d73
Eric Maranga <ekirira22@gmail.com>
2024-03-10 00:07:15 +0300

    completed assessment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+import ipdb
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagram_list):
+        matches = []
+        for word in anagram_list:
+            if sorted(word) == sorted(self.word):
+                matches.append(word)
+        return matches
+
+
+listen = Anagram("listen")
+print(listen.match(['enlist', 'google', 'inlets', 'banana']))
